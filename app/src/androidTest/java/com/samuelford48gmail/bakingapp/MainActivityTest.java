29587e2a26d10fe120ee6a892aa1eeda912acf08
Samuelford24 <Samuelford48@gmail.com>
2020-06-20 11:55:38 -0400

    update

--- app/src/androidTest/java/com/samuelford48gmail/bakingapp/MainActivityTest.java
+++ app/src/androidTest/java/com/samuelford48gmail/bakingapp/MainActivityTest.java
@@ -6,8 +6,10 @@ import android.app.Instrumentation;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.IdlingResource;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
@@ -18,40 +20,39 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Map;
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.Intents.intending;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static java.util.regex.Pattern.matches;
+
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
-}
-/*
-    @Rule
-    public ActivityTestRule<MenuActivity> mActivityTestRule =
-            new ActivityTestRule<>(MenuActivity.class);
-
-    private IdlingResource mIdlingResource;
-
-
-    // Registers any resource that needs to be synchronized with Espresso before the test is run.
 
 
     @Test
-    public void idlingResourceTest() {
-        onData(anything()).inAdapterView(withId(R.id.tea_grid_view)).atPosition(0).perform(click());
-    }
+    public void testRecylcerview() {
+        //  String output="Brownies";
+        //onView(withId(R.id.rvMain).);
+        //  onView(ViewMatchers.withId(R.id.rvMain)).perform(RecyclerViewActions.actionOnItemAtPosition(0, (ViewAction) containsString(output)));
+        // .perform(RecyclerViewActions.scrollToPosition(1)).check(withText(output).matches(isDisplayed()));
 
-    // Remember to unregister resources when not needed to avoid malfunction.
-    @After
-    public void unregisterIdlingResource() {
-        if (mIdlingResource != null) {
-            Espresso.unregisterIdlingResources(mIdlingResource);
-        }
+        /// onView(withText(output)).check(matches(isDisplayed()));
+
     }
 }
-}
-*/
